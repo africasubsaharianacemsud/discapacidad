791ed9461268407c18e56e9e0bd76bb2cfe97f5f
cemunam <africasubsahariana.cemsud@gmail.com>
2022-03-17 02:18:06 +0000

    Add Translate Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'disacpacidad';
+
+  constructor(private translate: TranslateService) {
+    if(!document.cookie.includes('lang')) {
+      document.cookie = "lang=en"
+      window.location.href = '/'
+    } else {
+      const lang = document.cookie.substring(5, 7);
+      this.setAppLanguage(lang);
+    }
+  }
+
+  public setAppLanguage = (lang: string) => {
+    this.translate.setDefaultLang(lang);
+    // this.translate.use(this.translate.getBrowserLang()!);
+  }
 }
