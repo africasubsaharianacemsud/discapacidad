b7960c40c0d036483b2409cf6abca198b01b22d1
Ordaz Romero Sergio <orcheko@gmail.com>
2022-03-21 19:22:41 +0000

    Add translations and define properties to toolbar mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'disacpacidad';
+  public title = 'disacpacidad';
+  public isToolbarHiden: boolean = false;
 
   constructor(private translate: TranslateService) {
     if(!document.cookie.includes('lang')) {
@@ -22,5 +23,18 @@ export class AppComponent {
   public setAppLanguage = (lang: string) => {
     this.translate.setDefaultLang(lang);
     // this.translate.use(this.translate.getBrowserLang()!);
+  }
+
+  public hideToolbar = (e: Event) => {
+    // console.log(this.hideToolbar)
+    const element: HTMLInputElement = e.target as HTMLInputElement;
+    const className: string = element.className;
+    if(!className.includes('tb') && !className.includes('hamburguer')) {
+      this.isToolbarHiden = true;
+    }
+  }
+
+  public showToolbar = (isHidden: boolean) =>  {
+    this.isToolbarHiden = isHidden;
   }
 }
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @Input() hideToolbar!: boolean;
+  @Output() hideToolbarToParent = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
+    if(window.innerWidth <= 680) {
+      this.hideToolbar = true;
+    }
   }
 
+  public openToolbar = () => {
+    this.hideToolbar = false;
+    this.hideToolbarToParent.emit(this.hideToolbar);
+  }
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number): void {
+    if(width > 680)
+      this.hideToolbar = false;
+    else
+      this.hideToolbar = true;
+  }
 }
